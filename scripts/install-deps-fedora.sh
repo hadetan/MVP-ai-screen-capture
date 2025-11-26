7dd8862eca49814ba78c5b9b17d27378a97e0dad
hadetan <aquibsyed83@gmail.com>
2025-11-26 16:57:01 +0530

    chore: cleaned unwanted files

--- scripts/install-deps-fedora.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-echo "Installing build dependencies for Fedora (rpm and deb building)..."
-sudo dnf install -y rpm-build rpmdevtools redhat-rpm-config gcc-c++ pkgconfig gtk3-devel webkit2gtk3-devel openssl-devel libappindicator gtk3 libnotify
-echo "Dependencies installed. You might still need dpkg tools if you plan to build .deb packages on Fedora. Install dpkg by: sudo dnf install -y dpkg" 
